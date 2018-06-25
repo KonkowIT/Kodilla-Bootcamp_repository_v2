21eb242aa806f93f2ac77242d9f2d81515e93486
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-06-25 15:47:48 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -10,14 +10,15 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
-	}
+	}*/
 }
