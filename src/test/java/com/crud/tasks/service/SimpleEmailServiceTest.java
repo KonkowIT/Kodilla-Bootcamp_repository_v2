fcf82cecc8f3fceff92ab2734d611d319b9e04a5
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-07-12 00:39:33 +0200

    update

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("test@test.com", "test", "Test Message","");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        if(mail.getToCc().isEmpty()) {
+            mailMessage.setTo(mail.getMailTo());
+            mailMessage.setSubject(mail.getSubject());
+            mailMessage.setText(mail.getSubject());
+        } else {
+            mailMessage.setTo(mail.getMailTo());
+            mailMessage.setSubject(mail.getSubject());
+            mailMessage.setText(mail.getSubject());
+            mailMessage.setCc(mail.getToCc());
+        }
+
+        //when
+        simpleEmailService.send(mail);
+
+        //then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
